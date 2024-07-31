1c80c1db91a055dbd52fad8a5f2885a48e5fea02
Ehsan <ehsan@zeropoint.hr>
2024-07-31 10:55:15 +0500

    unique rule implemented

--- platform/plugins/real-estate/src/Http/Requests/CategoryDocumentRequest.php
+++ platform/plugins/real-estate/src/Http/Requests/CategoryDocumentRequest.php
@@ -3,6 +3,7 @@
 namespace Botble\RealEstate\Http\Requests;
 
 use Botble\Support\Http\Requests\Request;
+use Illuminate\Validation\Rule;
 
 class CategoryDocumentRequest extends Request
 {
@@ -10,7 +11,20 @@ class CategoryDocumentRequest extends Request
     {
         return [
             'category_id' => 'required',
-            'document_id' => 'required'
+            'document_id' => [
+                'required',
+                'integer',
+                Rule::unique('category_documents')->where(function ($query) {
+                    return $query->where('category_id', $this->category_id);
+                }),
+            ],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'document_id.unique' => 'Such a record already exists.',
         ];
     }
 }
