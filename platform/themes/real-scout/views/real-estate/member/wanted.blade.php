bf8133c21695fe76c50bf04b2e70e89db7127e66
Ehsan Elahi <ehsan.sani91@gmail.com>
2024-08-14 17:27:05 +0500

    wnated invenstment small refact

--- platform/themes/real-scout/views/real-estate/member/wanted.blade.php
+++ platform/themes/real-scout/views/real-estate/member/wanted.blade.php
@@ -240,7 +240,7 @@
                                     <div class="col-md-4 pl-1">
                                         <button style="height: 35px;" type="button" id="project"
                                             class="btn d-block w-100 label-secondary type_project" data-id="project"
-                                            data-type-name="PROJECT" style="Width:100%" value="project">Project
+                                            data-type-name="PROJECT" style="Width:100%" value="project">Invest
                                         </button>
                                     </div>
                                 </div>
