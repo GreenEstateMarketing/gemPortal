748ed177d851b706cf45f67d88b23f32284d04e4
Ehsan <ehsan@zeropoint.hr>
2024-08-19 12:37:28 +0500

    area made unique

--- platform/plugins/location/src/Http/Requests/CityAreaRequest.php
+++ platform/plugins/location/src/Http/Requests/CityAreaRequest.php
@@ -9,8 +9,15 @@ class CityAreaRequest extends Request
     public function rules()
     {
         return [
-            'city_area_name' => 'required',
+            'city_area_name' => 'required|unique:city_area,city_area_name,NULL,id,city_id,' . $this->input('city_id'),
             'city_id' => 'required',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'city_area_name.unique' => 'City area already exists in the selected city.'
         ];
     }
 }
