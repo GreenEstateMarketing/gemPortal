2f1b9c8a13496a2a8fe0882ed72367318f8b4f31
Ehsan <ehsan@zeropoint.hr>
2025-04-25 12:35:56 +0500

    logo changed

--- platform/plugins/real-estate/resources/views/account/components/header.blade.php
+++ platform/plugins/real-estate/resources/views/account/components/header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light bg-black bb b--black-10">
     <div class="container-fluid">
         @if (theme_option('logo'))
-          <a href="{{ url('/') }}"><img src="{{ Theme::asset()->url('images/gemlogo.png')  }}" alt="{{ theme_option('site_title') }}" height="35"></a>
+          <a href="{{ url('/') }}"><img src="{{ Theme::asset()->url('images/gemlogo-bk.png')  }}" alt="{{ theme_option('site_title') }}" height="35"></a>
         @else
           <div class="brand-container tc mr2 br2">
             <a class="navbar-brand b white ma0 pa0 dib w-100" href="{{ url('/') }}" title="{{ theme_option('site_title') }}">{{ ucfirst(mb_substr(theme_option('site_title'), 0, 1, 'utf-8')) }}</a>
