051f8c5aaa42aca5d59bdc943ba8d41818be3543
Ayesha Nazir <ayeshanazir4667f@gmail.com>
2026-09-15 23:26:29 -0700

    Blog page

--- platform/themes/flex-home/views/index.blade.php
+++ platform/themes/flex-home/views/index.blade.php
@@ -1,7 +1,24 @@
 @php Theme::layout('homepage') @endphp
 
 {!! do_shortcode('[featured-projects][/featured-projects]') !!}
+
 {!! do_shortcode('[properties-by-locations][/properties-by-locations]') !!}
+
 {!! do_shortcode('[properties-for-sale][/properties-for-sale]') !!}
+
 {!! do_shortcode('[properties-for-rent][/properties-for-rent]') !!}
+
 {!! do_shortcode('[latest-news][/latest-news]') !!}
+
+{{-- ================= BLOG SECTION ================= --}}
+
+{!! do_shortcode('
+    [blog-posts
+        title="Latest Real Estate Insights"
+        limit="3"
+        show_excerpt="yes"
+        show_meta="yes"
+    ][/blog-posts]
+') !!}
+
+{{-- ================= END BLOG SECTION ================= --}}
